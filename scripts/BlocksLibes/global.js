10c2b70b057dbf6230d6a037f851b4efbc513acf
NO9527 <82761898+no9527dada@users.noreply.github.com>
2022-10-01 12:47:16 +0800

    Delete scripts directory

--- scripts/BlocksLibes/global.js
+++
@@ -1,8 +0,0 @@
-const lib = require('lib');
-
-exports.techDsAvailable = () =>
-    Vars.state == null
-    || Vars.state.rules.infiniteResources
-    || !Vars.player
-    || Vars.player.team().cores().find(boolf(v => v.block.name == lib.modName + "-dimension-technology-core"))
-    || Vars.player.team().cores().find(boolf(v => v.block.name == lib.modName + "-dimension-technology-core-3"))
--- scripts/BlocksLibes/lib.js
+++
@@ -1,9 +0,0 @@
-
-exports.modName = "creators";
- exports.setBuilding = function(blockType, buildingCreator) {
-    blockType.buildType = prov(() => buildingCreator(blockType));
-}
-exports.cons2 = (func) => new Cons2({
-    get: (v1, v2) => func(v1, v2)
-});
-exports.int = (v) => new java.lang.Integer(v);
